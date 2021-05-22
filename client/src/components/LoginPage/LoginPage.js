ee0e8f47c8caceb5d3ed9bc0013c378ff8964ee2
Vika <vickiepav@gmail.com>
2021-05-22 19:23:22 +0200

    Bugs fix

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -1,22 +1,41 @@
-
+import { SocketContext } from '../../context/socket';
+import { UsersContext } from '../../context/users';
+import { MainContext } from '../../context/main';
+import { useHistory } from 'react-router-dom';
+import { useContext, useEffect } from 'react';
 import React from 'react'
 
 
-class LoginPage extends React.Component {
-	constructor() {
-		super()
-	}
-	render() {
+const LoginPage = () => {
+	const socket = useContext(SocketContext)
+	const history = useHistory()
+	const { name, setName, room, setRoom } = useContext(MainContext)
+	const { setUsers } = useContext(UsersContext)
+
+
+	useEffect(() => {
+		socket.on("users", users => {
+			setUsers(users)
+		})
+	})
+
+	const handleClick = () => {
+		socket.emit('login', { name, room }, error => {
+			if (error) {
+				console.log(error)
+			}
+			history.push('/chat')
+		})
 		console.log(1)
 		return (
 			<div>
-				<input placeholder="Имя Пользовате" />
+				<input placeholder="Имя Пользователя" />
 				<input placeholder="Имя комнаты" />
-				<button>Send</button>
+				<button onClick={handleClick}>Send</button>
 			</div>
 		)
+
 	}
+
 }
-
-
 export default LoginPage;
--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -1,11 +1,11 @@
 import io from "socket.io-client";
-
+import React from 'react'
 const SOCKET_URL = 'http://localhost:5000/'
 
 export const socket = io.connect(SOCKET_URL);
 export const SocketContext = React.createContext();
 
-const SocketApp = () => {
+const SocketApp = ({ children }) => {
 	return (
 		<SocketContext.Provider value={socket}>
 			{children}
@@ -13,4 +13,4 @@ const SocketApp = () => {
 	);
 };
 
-export { SocketContext, SocketApp }
+export { SocketApp }
